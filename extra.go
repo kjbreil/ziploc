d439e9daf180d397a2be82046b30acf851cfb571
Kjell Breiland <kjell.breiland@gmail.com>
2019-12-16 13:21:31 -0800

    fix lint problems

--- extra.go
+++ extra.go
@@ -7,14 +7,10 @@ import (
 )
 
 // writeInstall is a hack for now until a macro module is fleshed out
-func (o *Option) writeInstall() {
+func (o *Option) writeInstall() error {
 	s := macro.LineS("@FMT(CMP,@WIZGET(INSTALL_STEP)<3,'®FMT(CHR,26)');")
 	s += macro.LineS("@WIZRESET;")
 	s += macro.LineS("@EXEC(SQL=REFRESH_MENU);")
 
-	ioutil.WriteFile(o.makePath("", "INSTALL.SQL"), []byte(s), 0666)
-}
-
-func (o *Option) writeAbsorb() {
-	ioutil.WriteFile(o.makePath("Inbox", "ABSORB_LOAD.SQL"), []byte{}, 0666)
+	return ioutil.WriteFile(o.makePath("", "INSTALL.SQL"), []byte(s), 0666)
 }
