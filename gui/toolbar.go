97840a3a395259b80ba35c4a11025e8060eaf5c4
Kjell Breiland <kjell.breiland@gmail.com>
2020-12-31 08:31:00 -0800

    gui starting to come together

---
+++ gui/toolbar.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fyne.io/fyne"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+	"github.com/sqweek/dialog"
+	"log"
+)
+
+func (u *ui) createToolbar() *fyne.Container {
+
+	var items []fyne.CanvasObject
+
+	// Open button
+	items = append(items, widget.NewButton("Open", func() {
+		filename, err := dialog.File().Filter("Json", "json").Load()
+		if err != nil {
+			log.Panic(err)
+		}
+		u.loadJsonConfig(&filename)
+	}))
+
+	// a config is loaded so show buttons for interacting with the option
+	if u.option != nil {
+		// Option Name
+		items = append(items, widget.NewLabel(u.option.Name))
+		// open config form button
+		items = append(items, widget.NewButton("Config", func() {
+			cw := u.configWindow()
+			u.loadWindow(cw)
+		}))
+	}
+
+	container := fyne.NewContainerWithLayout(layout.NewHBoxLayout(), items...)
+
+	return container
+}
