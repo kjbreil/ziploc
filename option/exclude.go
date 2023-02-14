1e3418ef5f26bd080a79eb3c00692dd5e2696cc2
Kjell Breiland <breilandk@ncbpinc.com>
2023-02-14 10:05:24 -0800

    regex in exclude

--- option/exclude.go
+++ option/exclude.go
@@ -1,14 +1,27 @@
 package option
 
 import (
-	"strings"
+	"log"
+	"regexp"
 )
 
 func (o *Option) excluded(current string) bool {
-	for _, exclude := range o.Exclude {
-		if strings.EqualFold(exclude, current) {
+	for _, item := range o.Exclude {
+		reg, err := regexp.Compile("(?i)" + item)
+		if err != nil {
+			log.Panicf("regex could not compile %s: %v", item, err)
+		}
+
+		if reg.MatchString(current) {
 			return true
 		}
+
 	}
+
+	// for _, exclude := range o.Exclude {
+	// 	if strings.EqualFold(exclude, current) {
+	// 		return true
+	// 	}
+	// }
 	return false
 }
